e11ead2636234ac6d2aa02d6e083742437ea35ba
donib-irakihda <donib.irakihda@gmail.com>
2023-06-16 02:03:22 +0545

    add: added few more validation test for POST /dogs

--- src/test/dog.test.js
+++ src/test/dog.test.js
@@ -9,6 +9,16 @@ beforeAll(async () => {
   await sequelize.authenticate().then(() => sequelize.sync());
 });
 
+beforeEach(async () => {
+  // Create a dog with a unique name before each test
+  await Dog.create({
+    name: "my_dog",
+    color: "brown",
+    tail_length: 10,
+    weight: 20,
+  });
+});
+
 afterEach(async () => {
   // Delete all dogs created during the test
   await Dog.destroy({ where: {} });
@@ -41,7 +51,7 @@ describe("GET /dogs", () => {
 describe("POST /dogs", () => {
   test("should create a new dog", async () => {
     const newDog = {
-      name: "test_dog11",
+      name: "test_dog",
       color: "test_color",
       tail_length: 15,
       weight: 20,
@@ -53,4 +63,38 @@ describe("POST /dogs", () => {
     expect(res.body.message).toBe("Dog created successfully! ");
     expect(res.body.newDog).toEqual(expect.objectContaining(newDog));
   }, 600000);
+
+  test("should return an error if the dog name already exists", async () => {
+    const dogData = {
+      name: "my_dog",
+      color: "test2_color",
+      tail_length: 15,
+      weight: 20,
+    };
+    const res = await request(app).post("/dogs").send(dogData);
+
+    expect(res.status).toBe(400);
+    expect(res.body.error).toBe("A dog with the same name already exists.");
+  }, 600000);
+
+  test.only("should return error when provided invalid data", async () => {
+    const invalidData = {
+      name: "",
+      color: 25,
+      tail_length: -8,
+      weight: "abc",
+    };
+
+    const res = await request(app).post("/dogs").send(invalidData);
+
+    console.log(res.body);
+    expect(res.status).toBe(500);
+    expect(res.body.errors).toEqual(
+      expect.arrayContaining([
+        "Name is required!",
+        "Tail length must be a positive number",
+        "Weight must be a positive number",
+      ])
+    );
+  });
 });
